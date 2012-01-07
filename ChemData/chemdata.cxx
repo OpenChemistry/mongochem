9ed95bb95b6afe0c3dc3e1412d307041fd7c047d
Kyle Lutz <kyle.lutz@kitware.com>
2012-01-07 15:26:51 -0500

    Set the correct application name for ChemData

--- ChemData/chemdata.cxx
+++ ChemData/chemdata.cxx
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 
   QCoreApplication::setOrganizationName("Kitware");
   QCoreApplication::setOrganizationDomain("kitware.com");
-  QCoreApplication::setApplicationName("MoleQueue");
+  QCoreApplication::setApplicationName("ChemData");
   QCoreApplication::setApplicationVersion("0.1.0");
   QApplication app(argc, argv);
 
