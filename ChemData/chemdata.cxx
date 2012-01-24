a4d1b938035c9864d8ef66eac5feec051d281ced
Kyle Lutz <kyle.lutz@kitware.com>
2012-01-24 10:58:17 -0500

    Cleanup the ChemData main() function
    This removes some unneeded testing code and cleans
    up the main() function in the chemdata.cxx file.

--- ChemData/chemdata.cxx
+++ ChemData/chemdata.cxx
@@ -1,26 +1,25 @@
+/******************************************************************************
+
+  This source file is part of the ChemData project.
+
+  Copyright 2011 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
 #include <QtGui/QApplication>
 
 #include "mainwindow.h"
 
-#include <iostream>
-#include <mongo/client/dbclient.h>
-#include <mongo/client/gridfs.h>
-#include <QtCore/QString>
-#include <QtCore/QStringList>
-#include <QtCore/QFileInfo>
-#include <QtCore/QFile>
-#include <QtCore/QDebug>
-
-using namespace mongo;
-using std::cout;
-using std::endl;
-
-QString m_fileName = "/home/marcus/ssd/src/avogadro/testfiles/benzene.mold";
-QFileInfo info(m_fileName);
-
 int main(int argc, char *argv[])
 {
-
   QCoreApplication::setOrganizationName("Kitware");
   QCoreApplication::setOrganizationDomain("kitware.com");
   QCoreApplication::setApplicationName("ChemData");
@@ -29,60 +28,6 @@ int main(int argc, char *argv[])
 
   ChemData::MainWindow window;
   window.show();
+
   return app.exec();
-
-  DBClientConnection c;
-  try {
-    c.connect("localhost");
-    cout << "connected OK" << endl;
-  }
-  catch (DBException &e) {
-    cout << "caught " << e.what() << endl;
-    return 1;
-  }
-
-  QFile file("/home/marcus/SBIR_chem_data.csv");
-  file.open(QIODevice::ReadOnly);
-  file.readLine();
-  while (!file.atEnd()) {
-    QString tmp = file.readLine();
-    QStringList cmp = tmp.split(",");
-    if (cmp.size() == 5) {
-      qDebug() << "String:" << cmp[0];
-      BSONObj p = BSONObjBuilder().genOID()
-                                  .append("CAS", cmp[0].trimmed().toStdString())
-                                  .append("Set", cmp[1].trimmed().toStdString())
-                                  .append("Observed", cmp[2].toDouble())
-                                  .append("Predicted log Sw (MLR)", cmp[3].toDouble())
-                                  .append("Predicted log Sw (RF)", cmp[4].toDouble())
-                                  .obj();
-      c.insert("chem.import", p);
-    }
-    else
-      qDebug() << "Wrong size" << cmp.size() << tmp;
-  }
-
-  return 0;
-
-  GridFS gfs(c, "tutorial", "fs");
-  BSONObj ret = gfs.storeFile(m_fileName.toStdString(),
-                              info.fileName().toStdString());
-  BSONObj b = BSONObjBuilder().genOID()
-                              .appendElements(ret)
-            .append("fileInserted", "01")
-            .obj();
-
-  BSONObj p = BSONObjBuilder().genOID()
-                               .append("name", "Joe")
-             .append("age", 33)
-             .obj();
-  c.insert("tutorial.persons", p);
-  c.insert("tutorial.persons", b);
-
-  cout << "count: " << c.count("tutorial.persons") << endl;
-  auto_ptr<DBClientCursor> cursor =
-    c.query("tutorial.persons");//QUERY("age" << 33));
-  while (cursor->more())
-    cout << cursor->next().toString() << endl;
 }
-
