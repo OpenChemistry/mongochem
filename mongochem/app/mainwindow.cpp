3bc010546e58b835afa8fce50c2c4924458d3b7b
Chris Harris <chris.harris@kitware.com>
2013-02-08 12:37:05 -0500

    Add Avogadro GL Widget to MongoChem
    Allow 3D structures to be displayed in GL Widget.
    
    Change-Id: I266d2ed1381fa7fb91252f587c5386daf5f4ae97

--- mongochem/app/mainwindow.cpp
+++ mongochem/app/mainwindow.cpp
@@ -57,6 +57,8 @@
 
 #include <mongochem/plugins/pluginmanager.h>
 
+#include <avogadro/qtplugins/pluginmanager.h>
+
 #ifdef QTTESTING
 #include <pqTestUtility.h>
 #include <pqEventObserver.h>
@@ -144,103 +146,6 @@ public:
     painter->restore();
   }
 };
-
-#ifdef QTTESTING
-class XMLEventObserver : public pqEventObserver
-{
-  QXmlStreamWriter* XMLStream;
-  QString XMLString;
-
-public:
-  XMLEventObserver(QObject* p) : pqEventObserver(p)
-  {
-    this->XMLStream = NULL;
-  }
-  ~XMLEventObserver()
-  {
-    delete this->XMLStream;
-  }
-
-protected:
-  virtual void setStream(QTextStream* stream)
-  {
-    if (this->XMLStream) {
-      this->XMLStream->writeEndElement();
-      this->XMLStream->writeEndDocument();
-      delete this->XMLStream;
-      this->XMLStream = NULL;
-    }
-    if (this->Stream)
-      *this->Stream << this->XMLString;
-
-    this->XMLString = QString();
-    pqEventObserver::setStream(stream);
-    if (this->Stream) {
-      this->XMLStream = new QXmlStreamWriter(&this->XMLString);
-      this->XMLStream->setAutoFormatting(true);
-      this->XMLStream->writeStartDocument();
-      this->XMLStream->writeStartElement("events");
-    }
-  }
-
-  virtual void onRecordEvent(const QString& widget, const QString& command,
-                             const QString& arguments)
-  {
-    if(this->XMLStream) {
-      this->XMLStream->writeStartElement("event");
-      this->XMLStream->writeAttribute("widget", widget);
-      this->XMLStream->writeAttribute("command", command);
-      this->XMLStream->writeAttribute("arguments", arguments);
-      this->XMLStream->writeEndElement();
-    }
-  }
-};
-
-class XMLEventSource : public pqEventSource
-{
-  typedef pqEventSource Superclass;
-  QXmlStreamReader *XMLStream;
-
-public:
-  XMLEventSource(QObject* p): Superclass(p) { this->XMLStream = NULL;}
-  ~XMLEventSource() { delete this->XMLStream; }
-
-protected:
-  virtual void setContent(const QString& xmlfilename)
-  {
-    delete this->XMLStream;
-    this->XMLStream = NULL;
-
-    QFile xml(xmlfilename);
-    if (!xml.open(QIODevice::ReadOnly)) {
-      qDebug() << "Failed to load " << xmlfilename;
-      return;
-    }
-    QByteArray data = xml.readAll();
-    this->XMLStream = new QXmlStreamReader(data);
-  }
-
-  int getNextEvent(QString& widget, QString& command, QString& arguments)
-  {
-    if (this->XMLStream->atEnd())
-      return DONE;
-    while (!this->XMLStream->atEnd()) {
-      QXmlStreamReader::TokenType token = this->XMLStream->readNext();
-      if (token == QXmlStreamReader::StartElement) {
-        if (this->XMLStream->name() == "event")
-          break;
-      }
-    }
-    if (this->XMLStream->atEnd())
-      return DONE;
-
-    widget = this->XMLStream->attributes().value("widget").toString();
-    command = this->XMLStream->attributes().value("command").toString();
-    arguments = this->XMLStream->attributes().value("arguments").toString();
-    return SUCCESS;
-  }
-};
-#endif // QTTESTING
 
 } // end anonymous namespace
 
@@ -354,6 +259,11 @@ MainWindow::MainWindow()
                                       vtkCommand::AnnotationChangedEvent,
                                       this,
                                       SLOT(updateSelectionFilterModel()));
+
+  // Load the Avogadro plugins
+  Avogadro::QtPlugins::PluginManager *plugin =
+    Avogadro::QtPlugins::PluginManager::instance();
+  plugin->load();
 
   setupTable();
   connectToDatabase();
