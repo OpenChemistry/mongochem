8406b75c0aafb38a39d74eede6a46db9795d1ab1
Kyle Lutz <kyle.lutz@kitware.com>
2013-02-20 11:03:31 -0500

    Add QtTesting support to MongoChem
    This adds support for using the QtTesting framework to run GUI
    tests for MongoChem.
    
    One simple test was added which queries the database for a molecule
    and attempts to open its detail dialog.
    
    Also updates the RPC tests to work on the CDash@Home machines.
    
    Change-Id: Ied48ad892970d3ed19c3b86b1b7687e896a8fc6a

--- mongochem/app/main.cpp
+++ mongochem/app/main.cpp
@@ -31,7 +31,78 @@ int main(int argc, char *argv[])
   QCoreApplication::setApplicationVersion("0.1.0");
   QApplication app(argc, argv);
 
+  // process command line options
   QSettings settings;
+  QString testFile;
+  bool testExit = true;
+
+  const QStringList &arguments = app.arguments();
+  for (int i = 1; i < arguments.size(); i++) {
+    const QString &argument = arguments[i];
+
+    if (argument == "--server") {
+      if (i + 1 < arguments.size()) {
+        settings.setValue("hostname", arguments[++i]);
+      }
+      else {
+        qWarning("--server option requires argument");
+        return -1;
+      }
+    }
+    else if (argument == "--port") {
+      if (i + 1 < arguments.size()) {
+        settings.setValue("port", arguments[++i]);
+      }
+      else {
+        qWarning("--port option requires argument");
+        return -1;
+      }
+    }
+    else if (argument == "--collection") {
+      if (i + 1 < arguments.size()) {
+        settings.setValue("collection", arguments[++i]);
+      }
+      else {
+        qWarning("--collection option requires argument");
+        return -1;
+      }
+    }
+    else if (argument == "--user") {
+      if (i + 1 < arguments.size()) {
+        settings.setValue("user", arguments[++i]);
+      }
+      else {
+        qWarning("--user option requires argument");
+        return -1;
+      }
+    }
+    else if (argument == "--test-file") {
+      if (i + 1 < arguments.size()) {
+        testFile = arguments[++i];
+      }
+      else {
+        qWarning("--test-file option requires argument");
+        return -1;
+      }
+    }
+    else if (argument == "--test-no-exit") {
+#if QTTESTING
+      testExit = false;
+#else
+      qWarning(
+        "MongoChem called with --test-no-exit but testing is disabled.");
+      return -1;
+#endif
+    }
+    else if (argument == "--testing") {
+    }
+    else {
+      qWarning("Unknown command line option: '%s'", qPrintable(argument));
+      return -1;
+    }
+  }
+
+  // setup default options if they have not been specified
   if (!settings.contains("hostname"))
     settings.setValue("hostname", "localhost");
   if(!settings.contains("port"))
@@ -44,6 +115,15 @@ int main(int argc, char *argv[])
   // create main gui window
   MongoChem::MainWindow window;
   window.show();
+
+  if (!testFile.isEmpty()) {
+#ifdef QTTESTING
+    window.playTestLater(testFile, testExit);
+#else
+    qWarning("MongoChem called with --test-file but testing is disabled.");
+    return -1;
+#endif
+  }
 
 #ifdef MongoChem_ENABLE_RPC
   // create rpc listener
