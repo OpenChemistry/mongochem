165b7563fe5f51ba17bdd69dae4083348a23581f
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-04-05 00:58:21 -0400

    Customize CPack a little, get NSIS working
    We want CPack to use our version, rather than the default 0.1.1, add
    the license file and icon setup along with some useful links once
    everything is installed.
    
    Change-Id: I82462159e365fad6e0bbcedfbda073c3889d717c

---
+++ cmake/MongoChemCPack.cmake
@@ -0,0 +1,17 @@
+set(CPACK_PACKAGE_NAME "MongoChem")
+set(CPACK_PACKAGE_VERSION_MAJOR ${MongoChem_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${MongoChem_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${MongoChem_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION ${MongoChem_VERSION})
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "MongoChem")
+set(CPACK_RESOURCE_FILE_LICENSE "${MongoChem_SOURCE_DIR}/COPYING")
+
+set(CPACK_PACKAGE_EXECUTABLES "mongochem" "MongoChem")
+set(CPACK_CREATE_DESKTOP_LINKS "mongochem")
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/MongoChemCPackOptions.cmake.in"
+  "${MongoChem_BINARY_DIR}/MongoChemCPackOptions.cmake" @ONLY)
+set(CPACK_PROJECT_CONFIG_FILE
+  "${MongoChem_BINARY_DIR}/MongoChemCPackOptions.cmake")
+
+include(CPack)
