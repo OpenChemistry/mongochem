0af41d4e4a4bca534c2827dbbb6d405df7060820
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-04-06 14:26:10 -0400

    More packaging tweaks for CPack
    Change-Id: I9ebf9f312798995852eebca93a6b7b9d305246df

--- cmake/MongoChemCPack.cmake
+++ cmake/MongoChemCPack.cmake
@@ -4,11 +4,17 @@ set(CPACK_PACKAGE_VERSION_MINOR ${MongoChem_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${MongoChem_VERSION_PATCH})
 set(CPACK_PACKAGE_VERSION ${MongoChem_VERSION})
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "MongoChem")
+set(CPACK_PACKAGE_VENDOR "http://openchemistry.org/")
+set(CPACK_PACKAGE_DESCRIPTION
+  "Qt MongoDB desktop cheminformatics application.")
 
 if(APPLE)
   configure_file("${MongoChem_SOURCE_DIR}/COPYING"
     "${MongoChem_BINARY_DIR}/COPYING.txt" @ONLY)
   set(CPACK_RESOURCE_FILE_LICENSE "${MongoChem_BINARY_DIR}/COPYING.txt")
+  set(CPACK_PACKAGE_ICON
+    "${MongoChem_SOURCE_DIR}/mongochem/app/icons/mongochem.icns")
+  set(CPACK_BUNDLE_ICON "${CPACK_PACKAGE_ICON}")
 else()
   set(CPACK_RESOURCE_FILE_LICENSE "${MongoChem_SOURCE_DIR}/COPYING")
 endif()
